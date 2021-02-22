d15db8d5eb759b4bb639beac5c97034eb18c3a0c
axllashero <Maxim11052006@gmail.com>
2021-02-22 19:01:17 +0700

    lol

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p$14r4n5+q3x9!_i2b9v9q_6)t^t4mfud-zn7+=k-28sr-a+47'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['axllasthero.pythonanywhere.com']
 
 
 # Application definition
