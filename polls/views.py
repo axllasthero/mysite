21df914da8c9e03fa4d5c6daac2c023441f3be33
axllashero <Maxim11052006@gmail.com>
2021-02-27 17:32:18 +0700

    add meme

--- polls/views.py
+++ polls/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse("<h1>Dont type http://127.0.0.1:8000/polls/meme/</h1>")
 
 def meme(request):
-    return HttpResponse('<h1>Look Lois i am Django hosted site</h1><br><img src="https://upload.wikimedia.org/wikipedia/ru/5/5b/FamilyGuy_Peter_Griffin_72.jpg">')
+    return HttpResponse('<h1>why..... just why</h1><br><img src="https://img.ifunny.co/images/76b93d4f620e1db822719bbcf960103c8edbd136534e2de650c7bf281c4822a9_1.jpg">')
